873ab8676457c26850a2cb9f15174ce0d60d7af9
OlegAngelo <naubalagtas@gmail.com>
2023-04-23 21:37:59 +0800

    Learning NextJS - Assets, Metadata, and CSS: Polishing Layout

---
+++ components/layout.js
@@ -0,0 +1,6 @@
+// component level common functions
+import styles from './layout.module.css';
+
+export default function Layout({ children }) {
+  return <div className={styles.container}>{children}</div>;
+}
